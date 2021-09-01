e196dd2cdb3d6b8ecb8107bcf64f0ae6df7524cf
Eric De Maria <edemaria@mailo.com>
2021-09-01 19:19:56 +0200

    Add color picker in html & js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* Variables */
 var form_details; //DOM element of the details form
-var signDOM
+var signDOM;
+var primaColor; //Input element of primary color
+var secondColor; //Input element of secondary color
+var internCSS;
 
 /* Initialisation */
 window.onload = function(){
@@ -8,28 +11,36 @@ window.onload = function(){
     //Get elements
     form_details = document.querySelector("#details");
     signDOM = document.querySelector("#emailSign");
+    primaColor = document.querySelector("#primaColor");
+    secondColor = document.querySelector("#secondColor");
+    internCSS = document.styleSheets[1]
 
     //Listeners
-    form_details.addEventListener('click', Transpose);
+    form_details.addEventListener('click', Transpose); //Click on send button in the details form
+    primaColor.addEventListener('change',ChangeColors); //Change on the primary color input
+    secondColor.addEventListener('change',ChangeColors); //Change on the secondary color input
+
+    ChangeColors();
+    Transpose();
 };
 
 /* Functions */
 
-///Transposes input on the template
+///Transposes data entered on the template
 function Transpose(event){
-    
     var inputsForm = form_details.querySelectorAll("input[type=text]");
-
     var signElement
-    inputsForm.forEach(element => {
 
+    //Transpose every txt input to the corresponding element of th template
+    inputsForm.forEach(element => {
         signElement = document.querySelector("#sign_"+element.getAttribute("id").substring(6));
-        if (signElement !== null){ 
-            signElement.innerHTML = element.value;
-            console.log("OK",element.getAttribute("id").substring(6))
-        }else{
-            console.log("NOK",element.getAttribute("id").substring(6))
-        }
-        
+        if (signElement !== null){ signElement.innerHTML = element.value; } //Verify if element exist on template
     });
+}
+
+///Change colors on the template
+function ChangeColors(){
+    internCSS.cssRules[0].style.color = primaColor.value;
+    internCSS.cssRules[0].style.borderColor  = primaColor.value;
+    internCSS.cssRules[1].style.color = secondColor.value;
 }

d53c2dd6dd0530bb5ca83629481b278af0b886e3
Eric De Maria <edemaria@mailo.com>
2021-09-03 16:29:35 +0200

    Add empty entries management

--- script.js
+++ script.js
@@ -24,34 +24,48 @@ window.onload = function(){
 
 ///Transposes data entered on the template
 function Transpose(){
-    var inputsForm = form_details.querySelectorAll("input");
+
     var signElement; //Template current element
 
     //Transpose every inputs to the corresponding element of the template
-    inputsForm.forEach(element => {
+    form_details.querySelectorAll("input").forEach(element => {
 
         //Get the corresponding template element of the current input
         signElement = document.querySelector("#sign_"+element.id.substring(6));
+        if (signElement == null) {return;} //Move to the next element if noexistent
 
-        
         //Different execution according to the input type
-        console.log(element.id, element.type)
         switch(element.type){
             
             //Change the HTML syntaxe
-            case "text": 
-                if (signElement == null){break;} //Move to the next if noexistent
-                signElement.innerHTML = element.value;
+            case "text" :  
+
+                //Hide if empty or show it
+                if (element.value == '') {signElement.hidden = 1;
+                }else{signElement.hidden = 0;}
+
+                signElement.innerHTML = element.value; //Transpose text
+                
                 break;
 
-            //Upload image in base64 if it selectionned
+            //Upload image
             case "file" :
-                if(element.value==""){break;} //Verifies that an image is selected 
-                const img = signElement; //In a constant because readAsDataURL is asynchrone function.
-                const reader = new FileReader();
-                const file = element.files[0];
-                reader.readAsDataURL(file);
-                reader.onload = function(e) {img.src = reader.result;}; //Show the img at the end of the loading because readAsDataURL function is asynchrone
+
+                //Verifies that an image is selected and show it
+                if(element.files.length==0){ 
+                    signElement.hidden = 1;                    
+                }else{
+                    signElement.hidden = 0;
+                    
+                    //Upload image in base64
+                    const img = signElement; //In a constant because readAsDataURL is asynchrone function.
+                    const reader = new FileReader();
+                    const file = element.files[0];
+                    reader.readAsDataURL(file);
+                    reader.onload = function(e) {img.src = reader.result;}; //Show the img at the end of the loading because readAsDataURL function is asynchrone
+
+                }
+                
                 break;
             
             //Change intern CSS color values
